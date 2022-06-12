996b5dc9e0e75edac192fe6368a8f37e7fa98b94
Mark Yavorskyi <yavorskiymark@gmail.com>
2022-06-12 17:36:10 +0300

    Done: created util files. Fixed: downgrade kotlin version from 1.7.0 to 1.6.21 due to compose version

---
+++ core/src/main/java/com/example/core/util/ViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.example.core.util
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+typealias ViewModelCreator<VM> = () -> VM
+
+class ViewModelFactory<VM : ViewModel>(
+    private val viewModelCreator: ViewModelCreator<VM>
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return viewModelCreator() as T
+    }
+
+}
