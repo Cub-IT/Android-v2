363900a82e018da78a749fb36f290e688fb8b6e0
Mark Yavorskyi <yavorskiymark@gmail.com>
2022-06-18 19:23:32 +0300

    Done: deleted unused fiels

--- app/src/main/java/com/example/cubit/navigation2/nav_target/NavTarget.kt
+++
@@ -1,9 +0,0 @@
-/*
-package com.example.cubit.navigation2.nav_target
-
-sealed class NavTarget {
-    object Back : NavTarget()
-
-    open class Screen(val route: String) : NavTarget()
-}
-*/
--- app/src/main/java/com/example/cubit/navigation2/nav_target/ViewModelNavigator.kt
+++
@@ -1,8 +0,0 @@
-/*
-package com.example.cubit.navigation2.nav_target
-
-import androidx.lifecycle.ViewModel
-
-interface ViewModelNavigator {
-    fun getViewModel(): ViewModel
-}*/
